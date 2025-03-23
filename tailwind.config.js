d1f0f400cff49d11ed68046dc32c7b40fdbb68bf
mspinard <mspinard@vulog.com>
2025-03-23 12:29:04 +0100

    setup ok

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    // Ajoutez ici les chemins vers vos fichiers source
+    './src/**/*.{html,js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
