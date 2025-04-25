80c38d467820da9194e6b784a9978147f76d5db0
mspinard <mspinard@vulog.com>
2025-04-25 14:23:15 +0200

    ok

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 3001;
 const corsOptions = {
   origin:
     process.env.NODE_ENV === 'production'
-      ? ['https://codebymax-dev.com']
+      ? ['http://codebymax-dev.com']
       : ['http://localhost:3000', 'http://localhost:5173'],
   methods: ['GET', 'POST'],
   allowedHeaders: ['Content-Type'],
